e815735880286df85ee3e77e69e9d774ccf2b2a6
Klea Cengu <3cengk35@solent.ac.uk>
2018-10-16 11:03:20 +0100

    mock tca update

--- myPracticeCourseWork/week4/mock-tca1/hotellock-parent/lock-reception/src/main/java/solent/ac/uk/ood/examples/hotellock/reception/HotelReceptionServiceImpl.java
+++ myPracticeCourseWork/week4/mock-tca1/hotellock-parent/lock-reception/src/main/java/solent/ac/uk/ood/examples/hotellock/reception/HotelReceptionServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import solent.ac.uk.ood.examples.hotellock.model.CardKey;
 import solent.ac.uk.ood.examples.hotellock.model.HotelReceptionService;
 import solent.ac.uk.ood.examples.hotellock.model.SecretKeyProvider;
+import solent.ac.uk.ood.examples.hotellock.secretkey.SecretKeyProviderImpl;
 
 /**
  *
@@ -20,13 +21,32 @@ public class HotelReceptionServiceImpl implements HotelReceptionService {
     
     @Override
     public String createCardCode(String roomNumber, Date startDate, Date endDate) {
-         
-        
-    }
+       
+        //String roomNumber = "100a";
+        int issueNumber = 01;
+        //Date startDate = new Date();
+        //Date endDate = new Date(startDate.getTime() + 1000 * 60 * 60 * 24); // 1 day later
 
+        CardKey cardKey = new CardKey();
+        cardKey.setRoomNumber(roomNumber);
+        cardKey.setIssueNumber(issueNumber);
+        cardKey.setStartDate(startDate);
+        cardKey.setEndDate(endDate);
+        System.out.println(cardKey);
+        String CardCode;
+        
+       // SecretKeyProvider keyProvider = new SecretKeyProviderImpl();
+        String cardString = secretKeyProvider.encodeCard(cardKey);
+        
+        return cardString;
+        
+        
+        
+     
+}
     @Override
-    public CardKey readCard(String cardCode) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public CardKey readCard(String cardString) {
+        CardKey decodedCardKey = secretKeyProvider.decodeCard(cardString);
     }
 
     @Override
